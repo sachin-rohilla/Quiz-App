2d95c7305c3d75f4ab0f11100314bd63606a3a53
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-10 22:18:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
-
+import { SiRepublicofgamers } from "react-icons/si";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-96 h-96 border p-4 shadow-lg rounded-lg">
+          <h1 className="flex items-center justify-center gap-2 font-semibold text-xl ">
+            Quiz App
+            <SiRepublicofgamers className="text-3xl" />
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
